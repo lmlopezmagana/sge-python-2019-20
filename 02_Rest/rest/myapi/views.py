dcd79d8c0aca76d4d15cde7e0726f4c5fce25181
Luis Miguel López Magaña <luismi.lopez@salesianos.edu>
2019-12-05 09:04:57 +0100

    Rest Framework

---
+++ 02_Rest/rest/myapi/views.py
@@ -0,0 +1,13 @@
+# from django.shortcuts import render
+from astroid.scoped_nodes import objects
+from rest_framework import viewsets
+
+from .models import Hero
+from .serializers import HeroSerializer
+
+# Create your views here.
+
+
+class HeroViewSet(viewsets.ModelViewSet):
+    queryset = Hero.objects.all().order_by('name')
+    serializer_class = HeroSerializer
